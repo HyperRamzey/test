0df21fc182791f5c0d41b45203a1931b5c3e340c
Sanjxz <36453187+HyperRamzey@users.noreply.github.com>
2025-04-29 08:55:46 +0300

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -12,7 +12,6 @@ local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.IgnoreGuiInset = true
 ScreenGui.Parent = playerGui
 
-print("ScreenGui Successful")
 
 local frame = Instance.new("Frame")
 frame.Parent = ScreenGui
@@ -20,18 +19,16 @@ frame.Size = UDim2.new(1,0,1,0)
 frame.BackgroundTransparency = 0
 frame.BackgroundColor3 = Color3.fromRGB(0,20,40)
 
-print("frame Successful")
 
 local textLabel = Instance.new("TextLabel")
 textLabel.Size = UDim2.new(1,0,1,0)
 textLabel.BackgroundColor3 = Color3.fromRGB(0,20,40)
 textLabel.Font = Enum.Font.GothamBold
 textLabel.TextColor3 = Color3.new(.8,.8,.8)
-textLabel.Text = "Loading Pet Spawner v1"
-textLabel.TextSize = 30
+textLabel.Text = "Loading X-Hub"
+textLabel.TextSize = 32
 textLabel.Parent = frame
 
-print("TextLabel Successful")
 
 local loadingRing = Instance.new("ImageLabel")
 loadingRing.Size = UDim2.new(0,256,0,256)
@@ -41,16 +38,13 @@ loadingRing.AnchorPoint = Vector2.new(0.5,0.5)
 loadingRing.Position = UDim2.new(0.5,0,0.5,0)
 loadingRing.Parent = frame
 
-print("LoadingRing Successful")
 
 ReplicatedFirst:RemoveDefaultLoadingScreen()
 
-print("Remove Deafult loading screen Unsuccessful")
 
 local tweenInfo = TweenInfo.new(4,Enum.EasingStyle.Linear,Enum.EasingDirection.In,-1)
 local tween = TweenService:Create(loadingRing,tweenInfo,{Rotation = 360})
 
-print("TweenService Successful")
 
 tween:Play()
 
